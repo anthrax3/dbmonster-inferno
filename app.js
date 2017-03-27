e0bbf254aca0a39a6e1d1467a28d0bf77b20937d
Sampo Kivistö <sampo.kivisto@visma.com>
2017-03-27 22:51:13 +0300

    Update to Inferno 1.5

--- app.js
+++ app.js
@@ -4,28 +4,10 @@ import {render, options, createVNode} from 'inferno';
 var elem = document.getElementById('App');
 options.recyclingEnabled = true; // Advanced optimisation
 
+
 perfMonitor.startFPSMonitor();
 perfMonitor.startMemMonitor();
 perfMonitor.initProfiler('view update');
-
-var tableProps = {
-	className: 'table table-striped latest-data'
-};
-var dbName = {
-	className: 'dbname'
-};
-var dbQueryCount = {
-	className: 'query-count'
-};
-var foo = {
-	className: 'foo'
-};
-var popoverLeft = {
-	className: 'popover left'
-};
-var popoverContent = {
-	className: 'popover-content'
-};
 
 function renderBenchmark(dbs) {
 	var length = dbs.length;
@@ -36,28 +18,26 @@ function renderBenchmark(dbs) {
 		var lastSample = db.lastSample;
 		var children = new Array(7);
 
-		children[0] = createVNode(2, 'td', dbName, db.dbname, null, null, null, true);
-		children[1] = createVNode(2, 'td', dbQueryCount, createVNode(2, 'span', {
-			className: lastSample.countClassName
-		}, lastSample.nbQueries, null, null, null, true), null, null, null, true);
+		children[0] = createVNode(2, 'td', 'dbname', db.dbname, null, null, null, true);
+		children[1] = createVNode(2, 'td', 'query-count',
+			createVNode(2, 'span', lastSample.countClassName, lastSample.nbQueries, null, null, null, true),
+			null, null, null, true);
 
 		for (var i2 = 0; i2 < 5; i2++) {
 			var query = lastSample.topFiveQueries[i2];
 
-			children[i2 + 2] = createVNode(66, 'td', {
-				className: query.elapsedClassName
-			}, [
-				createVNode(2, 'div', foo, query.formatElapsed, null, null, null, true),
-				createVNode(66, 'div', popoverLeft, [
-					createVNode(2, 'div', popoverContent, query.query, null, null, null, true),
-					createVNode(2, 'div', { className: 'arrow' }, null, null, null, null, true)
+			children[i2 + 2] = createVNode(66, 'td', query.elapsedClassName, [
+				createVNode(2, 'div', 'foo', query.formatElapsed, null, null, null, true),
+				createVNode(66, 'div', 'popover left', [
+					createVNode(2, 'div', 'popover-content', query.query, null, null, null, true),
+					createVNode(2, 'div', 'arrow', null, null, null, null, true)
 				], null, null, null, true)
 			], null, null, null, true);
 		}
 		databases[i] = createVNode(66, 'tr', null, children, null, null, null, true);
 	}
 
-	render(createVNode(2, 'table', tableProps, createVNode(66, 'tbody', null, databases, null, null, null, true), null, null, null, true), elem);
+	render(createVNode(2, 'table', 'table table-striped latest-data', createVNode(66, 'tbody', null, databases, null, null, null, true), null, null, null, true), elem);
 }
 
 function start() {
