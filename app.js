b6bcde6a021388e4d3ff304a7d4b55f63a9ef0da
Sampo Kivistö <sampo.kivisto@live.fi>
2018-02-11 19:59:04 +0200

    Inferno v4

--- app.js
+++ app.js
@@ -1,52 +1,52 @@
 import {ENV} from './ENV';
-import {render, options, version, createVNode} from 'inferno';
+import {render, createTextVNode, version, createVNode} from 'inferno';
 
 document.title = 'Inferno ' + version + ' dbMonster';
 
 var elem = document.getElementById('App');
-options.recyclingEnabled = true; // Advanced optimisation
-
 
 perfMonitor.startFPSMonitor();
 perfMonitor.startMemMonitor();
 perfMonitor.initProfiler('view update');
 
 function renderBenchmark(dbs) {
-	var length = dbs.length;
-	var databases = new Array(length);
+    var length = dbs.length;
+    var databases = new Array(length);
 
-	for (var i = 0; i < length; i++) {
-		var db = dbs[i];
-		var lastSample = db.lastSample;
-		var children = new Array(7);
+    for (var i = 0; i < length; i++) {
+        var db = dbs[i];
+        var lastSample = db.lastSample;
+        var children = new Array(7);
 
-		children[0] = createVNode(2, 'td', 'dbname', db.dbname, null, null, null, true);
-		children[1] = createVNode(2, 'td', 'query-count',
-			createVNode(2, 'span', lastSample.countClassName, lastSample.nbQueries, null, null, null, true),
-			null, null, null, true);
+        children[0] = createVNode(1, 'td', 'dbname', createTextVNode(db.dbname), 2);
+        children[1] = createVNode(1, 'td', 'query-count',
+            createVNode(1, 'span', lastSample.countClassName, createTextVNode(lastSample.nbQueries), 2),
+            2);
 
-		for (var i2 = 0; i2 < 5; i2++) {
-			var query = lastSample.topFiveQueries[i2];
+        for (var i2 = 0; i2 < 5; i2++) {
+            var query = lastSample.topFiveQueries[i2];
 
-			children[i2 + 2] = createVNode(66, 'td', query.elapsedClassName, [
-				createVNode(2, 'div', 'foo', query.formatElapsed, null, null, null, true),
-				createVNode(66, 'div', 'popover left', [
-					createVNode(2, 'div', 'popover-content', query.query, null, null, null, true),
-					createVNode(2, 'div', 'arrow', null, null, null, null, true)
-				], null, null, null, true)
-			], null, null, null, true);
-		}
-		databases[i] = createVNode(66, 'tr', null, children, null, null, null, true);
-	}
+            children[i2 + 2] = createVNode(1, 'td', query.elapsedClassName, [
+                createVNode(1, 'div', null, createTextVNode(query.formatElapsed), 2),
+                createVNode(1, 'div', 'popover left', [
+                    createVNode(1, 'div', 'popover-content', createTextVNode(query.query), 2),
+                    createVNode(1, 'div', 'arrow')
+                ], 4)
+            ], 4);
+        }
+        databases[i] = createVNode(1, 'tr', null, children, 4);
+    }
 
-	render(createVNode(2, 'table', 'table table-striped latest-data', createVNode(66, 'tbody', null, databases, null, null, null, true), null, null, null, true), elem);
+    render(
+        createVNode(1, 'table', 'table table-striped latest-data', createVNode(1, 'tbody', null, databases, 4), 2),
+        elem);
 }
 
-function start() {
-	var dbs = ENV.generateData(false).toArray();
-	perfMonitor.startProfile('view update');
-	renderBenchmark(dbs);
-	perfMonitor.endProfile('view update');
-	setTimeout(start, ENV.timeout);
+function loop() {
+    var dbs = ENV.generateData(false).toArray();
+    perfMonitor.startProfile('view update');
+    renderBenchmark(dbs);
+    perfMonitor.endProfile('view update');
+    setTimeout(loop, ENV.timeout);
 }
-start();
+loop();
